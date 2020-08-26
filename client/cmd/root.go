9d0a81db5ff8aae2c9c6ac232d45098b252e2d7c
hiro-hori <kazemachi3+github@gmail.com>
2020-08-26 17:30:03 +0900

    引数ちゃんとする。slack_urlは環境変数も対応する

--- client/cmd/root.go
+++ client/cmd/root.go
@@ -1,81 +1,68 @@
 package cmd
 
-
 import (
-	"os"
-	"fmt"
-	// "strings"
-	"encoding/json"
-
-	"net/http"
 	"bytes"
-	"time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 	"strconv"
-
-
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-func Execute() {
-	// var echoTimes int
+var (
+	// used for flags
+	slackUrl       string
+	serverUrl      string
+	confirmMessage string
 
-	// var cmdPrint = &cobra.Command{
-	// 	Use:   "print [string to print]",
-	// 	Short: "Print anything to the screen",
-	// 	Long: `print is for printing anything back to the screen.
-	// For many years people have printed back to the screen.`,
-	// 	Args: cobra.MinimumNArgs(1),
-	// 	Run: func(cmd *cobra.Command, args []string) {
-	// 		fmt.Println("Print: " + strings.Join(args, " "))
-	// 	},
-	// }
-
-	// var cmdEcho = &cobra.Command{
-	// 	Use:   "echo [string to echo]",
-	// 	Short: "Echo anything to the screen",
-	// 	Long: `echo is for echoing anything back.
-	// Echo works a lot like print, except it has a child command.`,
-	// 	Args: cobra.MinimumNArgs(1),
-	// 	Run: func(cmd *cobra.Command, args []string) {
-	// 		fmt.Println("Echo: " + strings.Join(args, " "))
-	// 	},
-	// }
-
-	// var cmdTimes = &cobra.Command{
-	// 	Use:   "times [string to echo]",
-	// 	Short: "Echo anything to the screen more times",
-	// 	Long: `echo things multiple times back to the user by providing
-	// a count and a string.`,
-	// 	Args: cobra.MinimumNArgs(1),
-	// 	Run: func(cmd *cobra.Command, args []string) {
-	// 		for i := 0; i < echoTimes; i++ {
-	// 		fmt.Println("Echo: " + strings.Join(args, " "))
-	// 		}
-	// 	},
-	// }
-
-	// cmdTimes.Flags().IntVarP(&echoTimes, "times", "t", 1, "times to echo the input")
-
-	var rootCmd = &cobra.Command{
+	rootCmd = &cobra.Command{
 		Use: "slack-approval",
+		// Args:
+		Short: `slack-approval enables group approval on slack`,
+		Long: `slack-approval enables group approval on slack.
+Please specify either --slack_url option or SLACK_APPROVAL_SLACK_URL environment variable.`,
+		Args: cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			confirmMessage, serverUrl, slackUrl := args[0], args[1], args[2]
-			fmt.Println("confirmMessage: " + confirmMessage)
-			fmt.Println("serverUrl: " + serverUrl)
-			fmt.Println("slackUrl: " + slackUrl)
+
+			if slackUrl = viper.GetString("slack_url"); slackUrl == "" {
+				cmd.Help()
+				os.Exit(255)
+			}
+			serverUrl += "/events"
+
 			id := createEvent(serverUrl)
 			requestToSlack(slackUrl, id, confirmMessage)
 			waitEventAndFinish(serverUrl, id)
 		},
 	}
-	// rootCmd.AddCommand(cmdPrint, cmdEcho)
-	// cmdEcho.AddCommand(cmdTimes)
+)
+
+func Execute() {
+
 	rootCmd.Execute()
 }
 
-// var debugUrl = "https://412c6cfaf8b6.ngrok.io"
-// var serverUrl = "https://8eiq8vncn4.execute-api.ap-northeast-1.amazonaws.com/dev/events"
+func init() {
+
+	viper.SetEnvPrefix("slack_approval")
+	viper.AutomaticEnv()
+	flags := rootCmd.Flags()
+	flags.StringVarP(&slackUrl, "slack_url", "", "", `slack incoming webhook URL.
+If you want to use envirionment variable, use SLACK_APPROVAL_SLACK_URL.`)
+	viper.BindPFlag("slack_url", flags.Lookup("slack_url"))
+
+	flags.StringVarP(&confirmMessage, "confirm_message", "", "", "(REQUIRED) confirm message on Slack")
+	viper.BindPFlag("confirm_message", flags.Lookup("confirm_message"))
+	rootCmd.MarkFlagRequired("confirm_message")
+
+	flags.StringVarP(&serverUrl, "server_url", "", "", "(REQUIRED) slack-approval server URL")
+	viper.BindPFlag("server_url", flags.Lookup("server_url"))
+	rootCmd.MarkFlagRequired("server_url")
+}
 
 type checkEventRespBody struct {
 	Status string
@@ -116,28 +103,28 @@ func checkEventStatus(serverUrl string, id string) string {
 }
 
 type createEventRespBody struct {
-    Id string
+	Id string
 }
 
 func createEvent(serverUrl string) string {
-	fiveMinutesLater := (time.Now().Unix()) + 60 * 5
+	fiveMinutesLater := (time.Now().Unix()) + 60*5
 
 	body := `
 	{
-		"timeout_epoch": `+ strconv.FormatInt(fiveMinutesLater, 10) +`
+		"timeout_epoch": ` + strconv.FormatInt(fiveMinutesLater, 10) + `
 	}`
 
 	res, err := http.Post(serverUrl, "application/json", bytes.NewBuffer([]byte(body)))
-    if err != nil {
-        panic(err)
+	if err != nil {
+		panic(err)
 	}
-	var data createEventRespBody
-    err = json.NewDecoder(res.Body).Decode(&data)
-
 	defer res.Body.Close()
 
-    if err != nil {
-        panic(err)
+	var data createEventRespBody
+	err = json.NewDecoder(res.Body).Decode(&data)
+
+	if err != nil {
+		panic(err)
 	}
 
 	fmt.Println("id: " + data.Id)
@@ -145,25 +132,15 @@ func createEvent(serverUrl string) string {
 }
 
 func requestToSlack(slackUrl, id string, message string) {
-	// url := "https://hooks.slack.com/services/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"
-
-	// jsonStr := []byte(`{"token":"aaaa"}`)
-	// req, err := http.Post(url, bytes.NewBuffer(jsonStr))
-
-	// client := &http.Client{
-		// CheckRedirect: redirectPolicyFunc,
-	// }
-
-	// http.Get(url)
 	res, err := http.Post(slackUrl, "application/json", bytes.NewBuffer([]byte(`
 	{
-		"text": "` + message + `",
+		"text": "`+message+`",
 		"blocks": [
 			{
 				"type": "header",
 				"text": {
 					"type": "plain_text",
-					"text": "` + message + `",
+					"text": "`+message+`",
 					"emoji": true
 				}
 			},
@@ -179,7 +156,7 @@ func requestToSlack(slackUrl, id string, message string) {
 			},
 			{
 				"type": "actions",
-				"block_id": "` + id + `",
+				"block_id": "`+id+`",
 				"elements": [
 					{
 						"type": "button",
@@ -205,10 +182,8 @@ func requestToSlack(slackUrl, id string, message string) {
 		]
 	}
 	`)))
-    defer res.Body.Close()
-
-    if err != nil {
-        panic(err)
-    }
-
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
 }
